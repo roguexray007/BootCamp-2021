cdcb10d4c17170673e2005d80d454d5833e07eab
Sachin Rai <87749655+rogers-razor@users.noreply.github.com>
2021-07-22 11:56:46 +0530

    Made changes in variable names and used slices
    Used slices and variadic functions and better variable names.

--- question3.go
+++ question3.go
@@ -23,30 +23,24 @@ func (contractor OnContractEmployee) findSalary() int{
 
 type FreelancerEmployee struct {
 	basicSalary int
-	numberOfHours int
+	numberOfHoursDaily int//number of hours should be entered on a daily basis
 	numberOfDays int
 }
 func (freelancer FreelancerEmployee) findSalary() int{
-	return freelancer.basicSalary * freelancer.numberOfHours * freelancer.numberOfDays
+	return freelancer.basicSalary * freelancer.numberOfHoursDaily * freelancer.numberOfDays
 }
 //function to calculate total Salary at The End of Month for all types of Employees
-func totalSalary(salaries ...int) int{
+func totalSalary(salaryMapOfEmployees ...Employee) int{
 	var totalSalaryAtMonthEnd int = 0
-	for _,totalSalaryOfEveryCategory := range salaries{
-		totalSalaryAtMonthEnd += totalSalaryOfEveryCategory
+	for _,totalSalaryOfEveryCategory := range salaryMapOfEmployees{
+		totalSalaryAtMonthEnd += totalSalaryOfEveryCategory.findSalary()
 	}
 	return totalSalaryAtMonthEnd
 }
 func main(){
-	var typeOfEmployee Employee
-	fulltime := FullTimeEmployee{500,28}
-	typeOfEmployee = fulltime //typeOfEmployee fulltime implements Employee
-	FullTimeEmployeeSalary := typeOfEmployee.findSalary()
+	fullTime := FullTimeEmployee{500,28}
 	contractor := OnContractEmployee{100,28}
-	typeOfEmployee = contractor //typeOfEmployee contractor implements Employee
-	OnContractEmployeeSalary := typeOfEmployee.findSalary()
 	freelancer := FreelancerEmployee{10,10,28}
-	typeOfEmployee = freelancer //typeOfEmployee freelancer implements Employee
-	FreelancerEmployeeSalary := typeOfEmployee.findSalary()
-	fmt.Println(totalSalary(FullTimeEmployeeSalary,OnContractEmployeeSalary,FreelancerEmployeeSalary))
+	salaryMapOfEmployees := []Employee{fullTime,contractor,freelancer}
+	fmt.Println(totalSalary(salaryMapOfEmployees...))
 }
